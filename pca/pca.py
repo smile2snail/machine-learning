0167c752088d1c88a878217737c61f876894955d
Jiayin Song <smile2snail@gmail.com>
2017-11-25 00:34:02 -0800

    use pca to do data compression and visualization

--- pca/pca.py
+++ pca/pca.py
@@ -1,29 +1,27 @@
 import numpy as np
 from sklearn.datasets import fetch_mldata
-import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 from ggplot import *
+import pandas as pd
+
+maxlen = 20000
 
 mnist = fetch_mldata("MNIST original")
-X = mnist.data / 255.0
-y = mnist.target
+X = (mnist.data / 255.0)[:maxlen]
+y = mnist.target[:maxlen]
+
 
 feat_cols = [ 'pixel'+str(i) for i in range(X.shape[1]) ]
+
 df = pd.DataFrame(X,columns=feat_cols)
 df['label'] = y
 df['label'] = df['label'].apply(lambda i: str(i))
 
 X, y = None, None
+
 rndperm = np.random.permutation(df.shape[0])
 
-# Plot the graph
-plt.gray()
-fig = plt.figure( figsize=(16,7) )
-for i in range(0,30):
-	ax = fig.add_subplot(3,10,i+1, title='Digit: ' + str(df.loc[rndperm[i],'label']) )
-	ax.matshow(df.loc[rndperm[i],feat_cols].values.reshape((28,28)).astype(float))
-plt.show()
 
 pca = PCA(n_components=3)
 pca_result = pca.fit_transform(df[feat_cols].values)
@@ -32,7 +30,8 @@ df['pca-one'] = pca_result[:,0]
 df['pca-two'] = pca_result[:,1] 
 df['pca-three'] = pca_result[:,2]
 
+
 chart = ggplot( df.loc[rndperm[:3000],:], aes(x='pca-one', y='pca-two', color='label') ) \
-		+ geom_point(size=75,alpha=0.8) \
-		+ ggtitle("First and Second Principal Components colored by digit")
-chart
+        + geom_point(size=75,alpha=0.8) \
+        + ggtitle("First and Second Principal Components colored by digit")
+chart.show()
